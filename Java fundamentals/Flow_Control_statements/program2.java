d1d170dd459ed3d092932d9a93a6324f8364a34b
MONISHA K <155455229+samplevhth@users.noreply.github.com>
2025-08-01 12:54:40 +0530

    program2.java

---
+++ Java fundamentals/Flow_Control_statements/program2.java
@@ -0,0 +1,14 @@
+public class program2
+  {
+    public static void main(String args[])
+    {
+      int a=Integer.parseInt(args[0]);
+      int b=Integer.parseInt(args[1]);
+      if(a%10==b%10){
+        System.out.println("True");
+      }
+      else{
+        System.out.println("False");
+      }
+    }
+  }
